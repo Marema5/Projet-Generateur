3f3d190832003d27f4525a35f60022d3264d1763
Marema5 <awamarememane434@gmail.com>
2023-09-19 14:22:54 -1200

    first commit

---
+++ ex.js
@@ -0,0 +1,8 @@
+const cite =[
+    "Le succée, c'est d'aller d'èchec en èchec sans perdre",
+    "Le seul moyen de faire du bon travail est d'aimer ce que vous faites",
+    "Le bonheur est la clè du succèes. Si vous aimez ce que vous faites",
+    "La seule façon de faire du bonheur est d'aimer ce vous faites",
+    "Le succèe; c'est tomber sept fois et se relever huit",
+];
+const citation = document.getElementByI('LIST').innerHTML="Awa Mareme"  + randomcite 
